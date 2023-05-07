ed15ac026258d6f2dae49d596ffd91946a0f1d75
Ale866 <87303299+Ale866@users.noreply.github.com>
2023-05-07 18:22:10 +0200

    added banner logic

--- components/Banners.vue
+++ components/Banners.vue
@@ -1,18 +1,44 @@
-<script setup lang='ts'>
+<script setup >
+
+
 const banners = [
   "cali.png", "circuit.png", "karate.png", "kick.png", "ninjutsu.png"
 ]
+let currentBanner = ref(-1);
+let interval = null;
+
+function changeActiveBanner() {
+  currentBanner.value = currentBanner.value + 1;
+  currentBanner.value = currentBanner.value % 5;
+  console.log(currentBanner.value);
+}
+
+onMounted(() => {
+  changeActiveBanner()
+  tick()
+})
+
+function tick() {
+  interval = setInterval(() => {
+    changeActiveBanner()
+  }, 5000);
+}
+
+onBeforeUnmount(() => {
+  clearInterval(interval)
+})
+
 </script>
 
 <template>
   <div class="container">
     <!-- <div class="header"> -->
-      <!-- <h1>Le nostre promozioni</h1> -->
+    <!-- <h1>Le nostre promozioni</h1> -->
     <!-- </div> -->
     <div class="courses-container">
-      <div v-for="banner of banners" class="course-element">
+      <div v-for="(banner, i) of banners" class="banner-element">
         <div class="img">
-          <img class="image" :src="'courses-banner/' + banner" alt="">
+          <img class="image" :src="'courses-banner/' + banner" alt="" :class="{ active: currentBanner == i }">
         </div>
       </div>
     </div>
@@ -21,43 +47,32 @@ const banners = [
 
 <style scoped>
 .container {
-  /* background-color: var(--cyan); */
   width: 100%;
-  height: 60%;
+  min-height: 70%;
   display: flex;
   justify-content: space-evenly;
   flex-direction: column;
   gap: 100px;
 }
 
-.header {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-
 .courses-container {
+  margin-top: 30px;
   width: 100%;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   align-self: center;
-  /* flex-wrap: wrap; */
-  row-gap: 75px;
+  flex-wrap: wrap;
 }
 
 .img {
-  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   object-fit: contain;
 }
 
-.course-element {
-  /* flex: 20%; */
+.banner-element {
   height: 100%;
   display: flex;
   gap: 0;
@@ -66,7 +81,36 @@ const banners = [
   flex-direction: column;
 }
 
-.image{
-  height: 75%;
+.image {
+  height: 320px;
+  margin: 30px;
+}
+
+@media (width < 1500px) {
+  .courses-container {
+    margin-top: 30px;
+  }
+
+  .banner-element {
+    flex-basis: 33%;
+  }
+}
+
+.active {
+  animation: aa 5s infinite;
+}
+
+@keyframes aa {
+  from {
+    transform: scale(1);
+  }
+
+  75% {
+    transform: scale(1.2);
+  }
+
+  to {
+    transform: scale(1);
+  }
 }
 </style>
